d877492439a2b06bf9a587435da98a12b295d1d7
sherbw28 <sherbw28@outlook.jp>
2022-01-13 21:33:02 +0900

    Initial Commit

---
+++ FEQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FEQuiz
+//
+//  Created by 佐藤栄祐 on 2022/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
